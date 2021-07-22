9ecbab3a73c93ad638b9e720bb8402e4cf31864c
Leandro Amorim <leandroamorimpb@gmail.com>
2021-07-22 11:34:38 -0300

    Service layer, component registration

--- src/main/java/com/codetec/curso/resources/UsuarioResource.java
+++ src/main/java/com/codetec/curso/resources/UsuarioResource.java
@@ -1,21 +1,36 @@
 package com.codetec.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codetec.curso.entities.Usuario;
+import com.codetec.curso.services.UsuarioService;
 
 @RestController
 @RequestMapping(value =  "/usuarios")
 public class UsuarioResource {
+	
+	@Autowired
+	private UsuarioService service;
 
 	//método de teste, o tipo de resposta do entity é do tipo Usuario
 	@GetMapping
-	public ResponseEntity<Usuario> findAll(){
-		Usuario u = new Usuario(1L, "teste", "teste@teste.com", "85855252", "123");
+	public ResponseEntity<List<Usuario>> findAll(){
+		//Usuario u = new Usuario(1L, "teste", "teste@teste.com", "85855252", "123");
+		List<Usuario> lista = service.findAll();
 		
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(lista);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> buscarPorId(@PathVariable("id") Long id){
+		Usuario usuario = service.findById(id);
+		return ResponseEntity.ok().body(usuario);
 	}
 }
---
+++ src/main/java/com/codetec/curso/services/UsuarioService.java
@@ -0,0 +1,26 @@
+package com.codetec.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.codetec.curso.entities.Usuario;
+import com.codetec.curso.repositories.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+	
+	public List<Usuario> findAll(){
+		return repository.findAll();
+	}
+	
+	public Usuario findById(Long id) {
+		 Optional<Usuario> usuario = repository.findById(id);
+		 return usuario.get();
+	}
+}
