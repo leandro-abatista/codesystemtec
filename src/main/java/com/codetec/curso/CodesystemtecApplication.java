3aec844a83a9facd47069d0abb19546559e1a667
Leandro Amorim <leandroamorimpb@gmail.com>
2021-07-22 10:04:45 -0300

    Projeto criado

---
+++ src/main/java/com/codetec/curso/CodesystemtecApplication.java
@@ -0,0 +1,13 @@
+package com.codetec.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesystemtecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodesystemtecApplication.class, args);
+	}
+
+}
