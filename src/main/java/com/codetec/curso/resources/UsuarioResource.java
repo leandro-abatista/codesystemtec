7a2d1d1e6d8ad7c6e29a104d3dfdafe03b003a55
Leandro Amorim <leandroamorimpb@gmail.com>
2021-07-24 14:36:02 -0300

    User update

--- src/main/java/com/codetec/curso/resources/UsuarioResource.java
+++ src/main/java/com/codetec/curso/resources/UsuarioResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,5 +54,11 @@ public class UsuarioResource {
 	public ResponseEntity<Void> deletarUsuario(@PathVariable("id") Long id){
 		service.deletar(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Usuario> atualizarUsuario(@PathVariable("id") Long id, @RequestBody Usuario usuario){
+		usuario = service.atualizar(id, usuario);
+		return ResponseEntity.ok().body(usuario);
 	}
 }
--- src/main/java/com/codetec/curso/services/UsuarioService.java
+++ src/main/java/com/codetec/curso/services/UsuarioService.java
@@ -31,4 +31,17 @@ public class UsuarioService {
 	public void deletar(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public Usuario atualizar(Long id, Usuario usuario) {
+		//deixa o objeto monitorado e prepara para fazer qualquer ação com o objto
+		Usuario entity = repository.getOne(id);
+		atualizarDados(entity, usuario);
+		return repository.save(entity);
+	}
+
+	private void atualizarDados(Usuario entity, Usuario usuario) {
+		entity.setNome(usuario.getNome());
+		entity.setEmail(usuario.getEmail());
+		entity.setFone(usuario.getFone());
+	}
 }
